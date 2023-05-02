a15e4284fb57397efca1f607346d10e0c96b6bca
eutenhofome <andres.dcid@yahoo.com>
2023-05-02 07:37:09 -0400

    r

--- final-project/js/test.js
+++ final-project/js/test.js
@@ -10,28 +10,29 @@ if (test) {
 
 // establish tile class
 class Tile {
-    constructor(phrase, audio, translation, language) {
+    constructor(phrase, audio, translation, language, key) {
         this.phrase = phrase;
         this.audio = audio;
         this.translation = translation;
         this.language = language;
+        this.key = key;
     }
 }
 
 // Array of All pairs with audios
 let tileValues = [
-    ["Mi nombre es", "My name is", "audio/minombrees.mp3", "audio/mynameis.mp3"],
-    ["Yo soy", "I am", "audio/yosoy.mp3", "audio/iam.mp3"],
-    ["Yo me llamo", "I am called", "audio/yomellamo.mp3", "audio/iamcalled.mp3"],
-    ["Que te llamas", "What is your name", "audio/quetellamas.mp3", "audio/whatisyourname.mp3"],
-    ["Buenos días", "Good morning", "audio/buenosdias.mp3", "audio/goodmorning.mp3"],
-    ["Buenas tardes", "Good afternoon", "audio/buenastardes.mp3", "audio/goodafternoon.mp3"],
-    ["Buenas noches", "Goodnight", "audio/buenasnoches.mp3", "audio/goodnight.mp3"],
-    ["Adiós", "Goodbye", "audio/adios.mp3", "audio/goodbye.mp3"],
-    ["Bienvenido", "Welcome", "audio/bienvenido.mp3", "audio/welcome.mp3"],
-    ["Cómo estás", "How are you", "audio/comoestas.mp3", "audio/howareyou.mp3"],
-    ["Hasta Luego", "See you later", "audio/hastaluego.mp3", "audio/seeyoulater.mp3"],
-    ["Cuidate", "Take Care", "audio/cuidate.mp3", "audio/takecare.mp3"]
+    ["Mi nombre es", "My name is", "audio/minombrees.mp3", "audio/mynameis.mp3", "a"],
+    ["Yo soy", "I am", "audio/yosoy.mp3", "audio/iam.mp3", "b"],
+    ["Yo me llamo", "I am called", "audio/yomellamo.mp3", "audio/iamcalled.mp3", "c"],
+    ["Que te llamas", "What is your name", "audio/quetellamas.mp3", "audio/whatisyourname.mp3", "d"],
+    ["Buenos días", "Good morning", "audio/buenosdias.mp3", "audio/goodmorning.mp3", "e"],
+    ["Buenas tardes", "Good afternoon", "audio/buenastardes.mp3", "audio/goodafternoon.mp3", "f"],
+    ["Buenas noches", "Goodnight", "audio/buenasnoches.mp3", "audio/goodnight.mp3", "g"],
+    ["Adiós", "Goodbye", "audio/adios.mp3", "audio/goodbye.mp3", "h"],
+    ["Bienvenido", "Welcome", "audio/bienvenido.mp3", "audio/welcome.mp3", "i"],
+    ["Cómo estás", "How are you", "audio/comoestas.mp3", "audio/howareyou.mp3"], "j",
+    ["Hasta Luego", "See you later", "audio/hastaluego.mp3", "audio/seeyoulater.mp3"], "k",
+    ["Cuidate", "Take Care", "audio/cuidate.mp3", "audio/takecare.mp3", "l"]
 ]
 
 // init tile classes into tileinputs
@@ -63,7 +64,7 @@ function createTiles() {
         let inputsIndex = inputsIndexes[i];
         tileInputs.push(new Tile(values[inputsIndex][0],values[inputsIndex][2],values[inputsIndex][1], "sp"));
         tileInputs.push(new Tile(values[inputsIndex][1],values[inputsIndex][3],values[inputsIndex][0], "eng"))
-
+        console.log(tileInputs)
     }
 
 }
@@ -85,12 +86,14 @@ function shuffledArray(max) {
 // fill the tiles onto board
 function fillTiles() {
     let inputs = tileInputs;
+    console.log(inputs)
     shuffledArray(16);
     for (let i = 0; i < 16; i++) {
         index = tileIndexes[i];
         let tile = inputs[i];
         $(`#phrase${index}`).text(tile.phrase);
-        console.log("language:", tile.language)
+        console.log("KEY", tile.key)
+        $(`.boxinput${index}`).val(tile.key);
         if (tile.language == "sp") {
             $(`#box${index}`).attr("id", "sp");
             $('input[name=box'+ index + ']').attr("name","sp");
@@ -98,27 +101,32 @@ function fillTiles() {
         if (tile.language == "eng") {
             $(`#box${index}`).attr("id", "eng");
             $('input[name=box'+ index + ']').attr("name","eng");
-            console.log("changed name")
         }
 
     }
 }
 
 $(`.boxes`).on("click", "input[name=sp]", function() {
-    console.log("k")
-    // console.log($('input[name=eng]').val)
     if ($('input[name=eng]:checked').val() == null) {
         return
     }
-    console.log("other one is clicked")
+    checkTiles();
 });
 
 $(`.boxes`).on("click", "input[name=eng]", function() {
-    console.log("eng")
     if ($('input[name=eng]:checked').val() == null) {
         return
     }
+    checkTiles();
 });
+
+function checkTiles() {
+    let sp = $(`input[name=sp]:checked`);
+    let eng = $(`input[name=eng]#checked`);;
+    console.log($(`input[name=sp]:checked`))
+    console.log(sp.val);
+    console.log(eng.val)
+}
 
 const audio = new Audio("audio/buenosdias.mp3");
 
