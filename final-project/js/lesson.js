1ec295cd67a9047b19b79adba65a92179cd740bd
eutenhofome <andres.dcid@yahoo.com>
2023-05-02 16:17:40 -0400

    ee

--- final-project/js/lesson.js
+++ final-project/js/lesson.js
@@ -1,4 +1,8 @@
-const lessons = document.querySelectorAll(".lesson");
-
-const buttons = Array(document.querySelectorAll(".lessonslider"));
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+lazyLoad: true
+$('.lessonModule').owlCarousel({
+    items:4,
+    lazyLoad:true,
+    loop:true,
+    margin:10
+});
